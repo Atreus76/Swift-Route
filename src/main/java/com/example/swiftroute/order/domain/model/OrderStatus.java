c5878f8b0c8fc281d68efad6fa8b2c06c852469d
Atreus76 <mathious173@gmail.com>
2026-05-08 16:55:34 +0700

    Add abstract method for each enum

--- src/main/java/com/example/swiftroute/order/domain/model/OrderStatus.java
+++ src/main/java/com/example/swiftroute/order/domain/model/OrderStatus.java
@@ -1,10 +1,26 @@
 package com.example.swiftroute.order.domain.model;
 
 public enum OrderStatus {
-    PENDING,
-    CONFIRMED,
-    DISPATCHED,
+    PENDING {
+        public boolean canTransitionTo(OrderStatus next) {
+            return next == CONFIRMED || next == CANCELLED;
+        }
+    },
+    CONFIRMED {
+        public boolean canTransitionTo(OrderStatus next) {
+            return next == DISPATCHED || next == CANCELLED;
+        }
+    },
+    DISPATCHED {
+        public boolean canTransitionTo(OrderStatus next) {
+            return next == DELIVERED || next == FAILED;
+        }
+    },
     DELIVERED,
     CANCELLED,
-    FAILED
+    FAILED{
+        public boolean canTransitionTo(OrderStatus next) {
+            return next == CANCELLED; // No transitions allowed from FAILED
+        }
+    }
 }
